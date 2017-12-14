87d51b0ccd0ad80d86182fad96f019eac0ef84ea
Jonathan Watmough <jonathan.watmough@gmail.com>
2017-12-13 18:22:13 -0600

    Added timing and optimized part 2

--- day 13.cpp
+++ day 13.cpp
@@ -5,12 +5,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     int depth, range;
-    vector<int> depths, ranges, severity;
+    vector<int> depths, ranges;
     while (cin >> depth) {
         if (cin.peek() == ':') cin.get();
         cin >> range;
@@ -18,24 +19,28 @@ int main(int argc, char* argv[])
         ranges.push_back(range);
     }
 
-    int delay = 0;
-    while (1) {
-        int severity{ 0 };
-        for (int step = 0; step<depths.size(); ++step) {
-            // move player
-            severity += ((depths[step] + delay) % ((ranges[step] - 1) * 2)) == 0 ? max(1,depths[step] * ranges[step]) : 0;
-        }
-        if (severity == 0 ) break;
-        if (delay == 0) cout << "Part 1: " << severity - 1 << endl;
-        delay++;
+    int severity{ 0 };
+    for (int step = 0; step<depths.size(); ++step) {
+        severity += (depths[step] % ((ranges[step] - 1) * 2))==0 ? (depths[step]*ranges[step]) : 0;
     }
-    cout << "Part 2: " << delay << endl;
+    cout << "Part 1: " << severity << endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    auto delay{ 0 }, fail{ 0 };
+    for (;; ++delay) {
+        fail = 0;
+        for (auto step{ 0 }; fail == 0 && step < depths.size(); ++step)
+            fail = !((depths[step] + delay) % ((ranges[step] - 1) * 2));
+        if (fail == 0) break;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    cout << "Part 2: " << delay << " in " << elapsed_seconds.count()*1000 << " msec" << endl;
     return 0;
 }
 
 /*
 Output:
 Part 1: 1640
-Part 2: 3960702
-
+Part 2: 3960702 in 49.2783 msec
 */
