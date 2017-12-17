66de719b62b6a48689ae21085f81dd3c785b02b9
Jonathan Watmough <jonathan.watmough@gmail.com>
2017-12-17 00:27:36 -0600

    day 17 part 2

--- day_17.cpp
+++ day_17.cpp
@@ -16,14 +16,18 @@ int main()
     buffer.push_back(0);
     assert(sz == buffer.size());
     auto pos{0u};
-    for (auto i{1};i<2018;++i) {
-        pos = (pos + step) % buffer.size();
-        buffer.insert(begin(buffer)+pos+1,i);
+    for (auto i{1};i<=50000000;++i) {
+        pos = (pos + step) % sz; //buffer.size();
+//        assert(sz==buffer.size());
+        if (pos==0)
+            buffer.insert(begin(buffer)+pos+1,i);
         sz++;
-        assert(sz==buffer.size());
+//        assert(sz==buffer.size());
         pos++;// = (pos +1)% buffer.size();
     }
-    cout << buffer[(pos+1)%buffer.size()] << endl;
-
+    
+    cout << buffer[0] << " " << buffer[1] << endl;
+    //cout << buffer[(pos+1)%buffer.size()] << endl;
+    
     return 0;
 }
