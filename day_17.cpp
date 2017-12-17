185908ef8f4a6f71ccebf7d2abb8bacb9e522f33
Jonathan Watmough <jonathan.watmough@gmail.com>
2017-12-17 00:06:26 -0600

    part 1

--- day_17.cpp
+++ day_17.cpp
@@ -1,10 +1,29 @@
+// Advent of Code 2017
+// Day 17 - 
+
 #include <iostream>
+#include <vector>
+#include <cassert>
 using namespace std;
 
 int main()
 {
-    cout << "Hello clang World!" << endl;
+    // ???
+    vector<int> buffer;
 
-cout << cout << cout;
+    int step{335},sz{1};
 
+    buffer.push_back(0);
+    assert(sz == buffer.size());
+    auto pos{0u};
+    for (auto i{1};i<2018;++i) {
+        pos = (pos + step) % buffer.size();
+        buffer.insert(begin(buffer)+pos+1,i);
+        sz++;
+        assert(sz==buffer.size());
+        pos++;// = (pos +1)% buffer.size();
+    }
+    cout << buffer[(pos+1)%buffer.size()] << endl;
+
+    return 0;
 }
